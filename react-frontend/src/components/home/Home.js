be8fa41e27790243045bf259e7db0843eb2f3b56
Jennifer Hallquist <jnhallquist@gmail.com>
2018-05-29 21:23:10 -0700

    Add jumbotron to Home

--- react-frontend/src/components/home/Home.js
+++ react-frontend/src/components/home/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Button, Jumbotron } from 'react-bootstrap';
 
 const Home = () => (
   <div>
-    <h3>Hello World</h3>
+    <Jumbotron>
+      <h1>Hello, world!</h1>
+      <p>
+        This is a demo application whose front end has been built using
+        the React.js front-end library.
+      </p>
+      <p>
+        Using the navigation bar above, you may navigate to the Users page,
+        from which you may view and edit a demo user{"'"}s profile.
+      </p>
+      <p>
+        <Button bsStyle="primary" href="#">Source Code</Button>
+      </p>
+    </Jumbotron>
   </div>
 );
 
